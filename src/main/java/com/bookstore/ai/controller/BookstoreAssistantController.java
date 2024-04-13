085e60c165282ab6a3fdaf86aa13154387b7b3ce
Daniel Penelva <d4n.andrade@gmail.com>
2024-04-12 23:39:42 -0300

    Add exemplo  do método bookstoreReview() instanciando new PromptTemplate()

--- src/main/java/com/bookstore/ai/controller/BookstoreAssistantController.java
+++ src/main/java/com/bookstore/ai/controller/BookstoreAssistantController.java
@@ -2,6 +2,7 @@ package com.bookstore.ai.controller;
 
 import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,9 +27,23 @@ public class BookstoreAssistantController {
 
 
     // http://localhost:8080/bookstore/chat2/informations
+    // http://localhost:8080/bookstore/chat2/informations?message=Pode falar sobre a classe OpenAiChatClient?
     @GetMapping("/chat2/informations")
     public ChatResponse bookstoreChatEx2(@RequestParam(value = "message", defaultValue = "Faça um resumo sobre Spring AI?") String message){
         return chatClient.call(new Prompt(message));
+    }
+
+    // http://localhost:8080/bookstore/reviews
+    // http://localhost:8080/bookstore/reviews?book=O mundo de sofia
+    @GetMapping("/reviews")
+    public String bookstoreReview(@RequestParam(value = "book", defaultValue = "Dom Quixote") String book) {
+        PromptTemplate promptTemplate = new PromptTemplate("""
+                  Por favor, me forneça
+                  um breve resumo do livro {book}
+                  e também a biografia de seu autor.
+                """);
+        promptTemplate.add("book", book);
+        return this.chatClient.call(promptTemplate.create()).getResult().getOutput().getContent();
     }
 
 }
