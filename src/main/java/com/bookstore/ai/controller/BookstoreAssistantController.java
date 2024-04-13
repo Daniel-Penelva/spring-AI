56dc73804edc6da86ef43106ca44a370e15d3174
Daniel Penelva <d4n.andrade@gmail.com>
2024-04-12 23:27:36 -0300

    Add exemplo  do método bookstoreChatEx2() instanciando new Prompt()

--- src/main/java/com/bookstore/ai/controller/BookstoreAssistantController.java
+++ src/main/java/com/bookstore/ai/controller/BookstoreAssistantController.java
@@ -1,5 +1,7 @@
 package com.bookstore.ai.controller;
 
+import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +22,13 @@ public class BookstoreAssistantController {
     @GetMapping("/informations")
     public String bookstoreChat(@RequestParam(value = "message", defaultValue = "Faça um resumo sobre Spring AI?") String message){
         return chatClient.call(message);
+    }
+
+
+    // http://localhost:8080/bookstore/chat2/informations
+    @GetMapping("/chat2/informations")
+    public ChatResponse bookstoreChatEx2(@RequestParam(value = "message", defaultValue = "Faça um resumo sobre Spring AI?") String message){
+        return chatClient.call(new Prompt(message));
     }
 
 }
